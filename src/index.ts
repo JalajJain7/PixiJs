83b0d4664ca601a74ada393b9acb1441ec5929c9
anujpandey0212 <MS20BTECH11003@iith.ac.in>
2021-08-27 17:27:03 +0530

    magnet drawn

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as PIXI from 'pixi.js'
 import { Viewport } from 'pixi-viewport'
+import { Graphics } from 'pixi.js'
 
 const app = new PIXI.Application({
     view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -25,14 +26,34 @@ viewport
     .wheel()
     .decelerate()
 
-const sprite = viewport.addChild(new PIXI.Sprite(PIXI.Texture.WHITE))
-sprite.tint = 0xff0000
-sprite.width = sprite.height = 100
-sprite.position.set(100, 100)
+// const sprite = viewport.addChild(new PIXI.Sprite(PIXI.Texture.WHITE))
+// sprite.tint = 0xff0000
+// sprite.width = sprite.height = 100
+// sprite.position.set(100, 100)
 const clampy: PIXI.Sprite = PIXI.Sprite.from("clampy.png");
 
 clampy.anchor.set(0.5);
 
 clampy.x = app.screen.width / 2;
 clampy.y = app.screen.height / 2;
-viewport.addChild(clampy);
+// viewport.addChild(clampy);
+
+var w=(window.innerWidth/2)-200;
+var h=(window.innerHeight/2)-50;
+var w2=window.innerWidth/2;
+
+var south_pole=new Graphics();
+south_pole.beginFill(0xff0000);
+south_pole.drawRect(w,h,200,100);
+viewport.addChild(south_pole);
+var north_pole=new Graphics();
+north_pole.beginFill(0x2211ad);
+north_pole.drawRect(w2,h,200,100);
+viewport.addChild(north_pole);
+
+let text_1 = new PIXI.Text('S',{fontFamily : 'Arial', fontSize: 34, fill : 0x1d1c1c, align : 'center'});
+text_1.position.set(w+355,h+25);
+north_pole.addChild(text_1);
+let text_2 = new PIXI.Text('N',{fontFamily : 'Arial', fontSize: 34, fill : 0x1d1c1c, align : 'center'});
+text_2.position.set(w+25,h+25);
+south_pole.addChild(text_2);
